daimlertruck/dt-ui

chore: rebase with main branch
* updates all imports from @dt-ui/* to @dt-dds/*
* fixes build issues

Signed-off-by: afreire-laptop <hello@antoniofreire.me>

apps/docs/.storybook/themes/truckApi/shape.ts
@@ -1,7 +1,6 @@
-import { CustomTheme, defaultTheme } from '@dt-ui/themes';
-import { Theme, theme as defaultTheme } from '@dt-dds/react';
+import { CustomTheme, daimlertruckTheme } from '@dt-dds/themes';
 
-const { shape: defaultShape, radius } = defaultTheme;
+const { shape: defaultShape, radius } = daimlertruckTheme;
 
 export const shape: CustomTheme['shape'] = {
   ...defaultShape,

daimlertruck/DT-DDS

apps/docs/.storybook/themes/truckApi/shape.ts
@@ -1,5 +1,4 @@
-import { CustomTheme, defaultTheme } from '@dt-ui/themes';
-import { Theme, theme as defaultTheme } from '@dt-dds/react';
+import { CustomTheme, defaultTheme } from '@dt-dds/themes';
 
 const { shape: defaultShape, radius } = defaultTheme;
 