867fb3f32ad61921d4a67e0eecfcb1c42697f73b
dt-dds <dt-dds@daimlertruck.com>
2025-10-13 15:40:16 +0000

    chore: update and publish Storybook

---
+++ PR-56/assets/TextField.styled-ChlRHZGe.js
@@ -0,0 +1,150 @@
+import{f as a}from"./emotion-styled.browser.esm-DBghOYGd.js";const d=(t,e)=>({default:e.palette.surface.default,contrast:e.palette.surface.contrast,light:e.palette.surface.light})[t]||e.palette.surface.default,p=a.div`
+  ${({theme:t,isFloatingLabel:e=!0,hasPrefix:o=!1})=>`
+    display: flex;
+    flex-direction: column;
+    gap: ${t.spacing.spacing_20};
+    width: 100%;
+    position: relative;
+    
+
+    ${o?`
+    label {
+      left: 0;
+      padding-left: ${e?t.spacing.l:""};
+    }
+    `:""}
+
+    i {
+      color: ${t.palette.content.medium};
+    }
+
+    :has(input[disabled]) {
+      i, label > span {
+        color: ${t.palette.content.light};
+      }
+    }
+
+    :has(input[readonly]:not(input[disabled])) {
+      label, label > span {
+        color: ${e?t.palette.content.medium:t.palette.content.default};
+        }
+      }
+  `}
+
+  input[type="search"]::-webkit-search-decoration,
+  input[type="search"]::-webkit-search-cancel-button,
+  input[type="search"]::-webkit-search-results-button,
+  input[type="search"]::-webkit-search-results-decoration {
+    display: none;
+  }
+`,c=a.input`
+  ${({theme:t,isFloatingLabel:e,isSearchType:o})=>`
+    ${t.fontStyles.bodyMdRegular}
+    border: 0;
+    outline: 0;
+    width: 100%;
+    background-color: inherit;
+    
+
+
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    input:-webkit-autofill:active {
+        transition: background-color 5000s ease-in-out 0s;
+    }
+
+    &::placeholder {
+      color: ${e?"transparent":t.palette.content.medium};
+    }
+
+    padding: ${e&&!o?`${t.spacing.xs} 0 ${t.spacing.spacing_30} 0`:""};
+  `}
+`,u=a.div`
+  ${({theme:t,onClick:e})=>{const o=!!e;return`
+    display: flex;
+    cursor: ${o?"pointer":"default"};
+
+    ${o&&`
+        &:hover > i  {
+          color: ${t.palette.content.dark};
+        }
+      `}
+  
+  `}}
+`,f=a.div`
+  ${({theme:t,onClick:e})=>{const o=!!e;return`
+    display: flex;
+    cursor: ${o?"pointer":"default"};
+
+    ${o&&`
+        &:hover > i  {
+          color: ${t.palette.content.dark};
+        }
+      `}
+  `}}
+`,$=a.div`
+  padding-left: ${({theme:t})=>t.spacing.spacing_50};
+`,b=a.div`
+  ${({theme:t})=>`
+    cursor: pointer;
+    display: flex;
+
+    :hover > i {
+      color: ${t.palette.content.dark}
+    }
+  `}
+`,g=a.div`
+  ${({theme:t,isFloatingLabel:e,variant:o,backgroundFill:n,hasError:i})=>{const l=i?t.palette.error.default:t.palette.border.medium,r=i?t.palette.error.default:t.palette.informative.default;return`
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    min-width: 198px;
+    width: 100%;
+    height: 54px;
+    color: ${t.palette.content.default};
+    gap: ${t.spacing.spacing_30};
+    background-color: ${d(n,t)};
+    
+    padding-inline: ${t.spacing.spacing_40};
+
+    ${!e&&"margin-top: 8px"};
+
+    ${o==="outlined"?`border-radius: ${t.shape.formField};
+        border: 1px solid ${l};
+
+        &:focus-within,
+        &:hover:not([disabled]) { 
+          border: 1px solid ${r};
+        }
+
+        &:hover:([disabled]) { 
+          border: 1px solid ${l};
+        }
+      `:`border-radius: ${t.shape.formField} ${t.shape.formField} 0 0;
+        border-bottom: 1px solid ${l};
+        
+        &:focus-within,
+        &:hover:not([disabled]) { 
+          border-bottom: 1px solid  ${r};
+        }
+      `};
+
+    &:has(input[readonly]:not([disabled])) {
+      background-color: ${t.palette.surface.default};
+      color: ${t.palette.content.medium};
+    }
+
+    &:has(input[disabled]), &:has(input[disabled]) > * {
+      cursor: not-allowed;
+
+      &:hover {
+        border-color: ${l};
+      }
+
+      input::placeholder {
+        color: ${e?"transparent":t.palette.content.light};
+      }
+    }
+  `}}
+`;export{g as I,b as R,p as T,u as a,c as b,f as c,$ as d};
