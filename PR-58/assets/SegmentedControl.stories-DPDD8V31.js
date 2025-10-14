0b7cc676cea2aeb23f19405819cbf5f15e98f31c
dt-dds <dt-dds@daimlertruck.com>
2025-10-14 08:52:01 +0000

    chore: update and publish Storybook

---
+++ PR-58/assets/SegmentedControl.stories-DPDD8V31.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-CB_V9I5Y.js";import{S as e}from"./SegmentedControl-C9STj4PZ.js";import"./index-CTjT7uj6.js";import"./Icon-DG6KDu5A.js";import"./emotion-styled.browser.esm-DBghOYGd.js";import"./Tooltip-70ClJ52A.js";import"./index-BV8q0bTk.js";import"./index-Bta9fxMP.js";import"./emotion-react.browser.esm-Bwsi5GpN.js";import"./index-BAMY2Nnw.js";import"./iframe-C8rQ4mPM.js";import"./Portal-DUP4U3GD.js";import"./index-XjDa9cP3.js";import"./z-index-Ds5QkyqS.js";const j={title:"Data Display/SegmentedControl",component:e,argTypes:{},render:t=>o.jsxs(e,{...t,children:[o.jsx(e.Option,{iconCode:"list",label:"List",value:"list"}),o.jsx(e.Option,{iconCode:"grid_view",label:"Gallery",value:"gallery"}),o.jsx(e.Option,{iconCode:"view_column",label:"Columns",value:"columns"}),o.jsx(e.Option,{disabled:!0,iconCode:"table_rows",label:"Rows",value:"rows"}),o.jsx(e.Option,{label:"Diagram",value:"diagram"})]})},l={argTypes:{selectedValue:{options:["none","list","gallery","columns","rows","diagram"],control:{type:"select"}}},args:{showLabel:!1,selectedValue:"none",onChangeControl:t=>console.log("value changed to",t)}};var a,n,r;l.parameters={...l.parameters,docs:{...(a=l.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  argTypes: {
+    selectedValue: {
+      options: ['none', 'list', 'gallery', 'columns', 'rows', 'diagram'],
+      control: {
+        type: 'select'
+      }
+    }
+  },
+  args: {
+    showLabel: false,
+    selectedValue: 'none',
+    onChangeControl: (value: string) => console.log('value changed to', value)
+  }
+}`,...(r=(n=l.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const f=["Default"];export{l as Default,f as __namedExportsOrder,j as default};
