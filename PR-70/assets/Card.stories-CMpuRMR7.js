95fa73a44635032cd98dfc86b3010671cc164abb
dt-dds <dt-dds@daimlertruck.com>
2025-10-22 10:36:15 +0000

    chore: update and publish Storybook

---
+++ PR-70/assets/Card.stories-CMpuRMR7.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-CB_V9I5Y.js";import{T as p}from"./Typography-BwSM8y-N.js";import{C as t}from"./Card-BqI2pIWG.js";import"./index-CTjT7uj6.js";import"./index-sWjZLPe6.js";import"./emotion-styled.browser.esm-DBghOYGd.js";const x={title:"Data Display/Card",component:t,render:({title:a,description:n})=>r.jsxs(t,{children:[r.jsx(t.Title,{children:a}),r.jsx(t.Detail,{children:r.jsx(p,{color:"grey_100",children:n})})]})},e={args:{title:"Card Title",description:"This is a short description"}};var s,i,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'Card Title',
+    description: 'This is a short description'
+  }
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,x as default};
