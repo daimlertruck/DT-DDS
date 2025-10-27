540189eba97af05dce02c5dfcdb231088c08d619
dt-dds <dt-dds@daimlertruck.com>
2025-10-27 09:28:20 +0000

    chore: cleanup unused folders

--- PR-68/assets/Toggle.stories-K3a9bkBE.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-CB_V9I5Y.js";import{r as h}from"./index-CTjT7uj6.js";import{T as u,a as k,b as T}from"./Toggle.styled-CkybMFiB.js";import"./emotion-styled.browser.esm-DBghOYGd.js";const p=({dataTestId:s,label:r,isDisabled:t=!1,isChecked:c=!1,onClick:a})=>{const l=s??"toggle",m=`${l}-checkbox-id`,f=o=>{t||a(o)},x=o=>{t||o.key==="Enter"&&(o.preventDefault(),a(o))};return e.jsxs(u,{"data-testid":l,hasLabel:!!r,onClick:f,onKeyDown:x,children:[e.jsx(k,{checked:c,disabled:t,id:m,type:"checkbox"}),e.jsx(T,{}),e.jsx("span",{children:r})]})},j=s=>{const[r,t]=h.useState(!1),c=()=>{t(a=>!a)};return e.jsx(p,{...s,isChecked:r,onClick:c})},S={title:"Data Display/Toggle",component:p,argTypes:{},render:({...s})=>e.jsx(j,{...s})},n={args:{isDisabled:!1,label:"Toggle"}};var d,i,g;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
-  args: {
-    isDisabled: false,
-    label: 'Toggle'
-  }
-}`,...(g=(i=n.parameters)==null?void 0:i.docs)==null?void 0:g.source}}};const E=["Default"];export{n as Default,E as __namedExportsOrder,S as default};
--- PR-71/assets/Typography-DDcIJD0Q.js
+++
@@ -1,7 +0,0 @@
-import{j as g}from"./jsx-runtime-CB_V9I5Y.js";import{r as d}from"./index-CTjT7uj6.js";import{g as m}from"./index-B6tfhuuo.js";import{f as l,d as u}from"./emotion-styled.browser.esm-DBghOYGd.js";const x=l.p`
-  ${({theme:s,fontStyles:o,color:t})=>`
-    ${s.fontStyles[o]};
-    color: ${t==="unset"||t==="inherit"?t:s.colors[t]||m(t,s)};
-   ${s.responsiveFontStyles[o]}
-  `};
-`,h=d.forwardRef(({children:s,element:o,fontStyles:t,color:a="grey_90",dataTestId:e,style:p,id:n},y)=>{const f=u(),r=o,i=t??(f.fontStyles[r]?r:"bodyLgRegular");return g.jsx(x,{as:o,color:a,"data-testid":e??"typography",fontStyles:i,id:n,ref:y,style:p,children:s})});export{h as T};
