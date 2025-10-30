3907641862efe24c060568faccb3d567459bfd78
dt-dds <dt-dds@daimlertruck.com>
2025-10-30 09:12:15 +0000

    chore: update and publish Storybook

---
+++ PR-75/assets/Button.stories-CXU_JbMx.js
@@ -0,0 +1,46 @@
+import{j as t}from"./jsx-runtime-CB_V9I5Y.js";import{B as p}from"./Button-CcKZkJgX.js";import"./Button.styled-BhGEnxfn.js";import"./index-CTjT7uj6.js";import"./Spinner-CJLgD7D_.js";import"./index-CWvrgZ1b.js";import"./emotion-styled.browser.esm-DBghOYGd.js";import"./Typography-DHEP7O00.js";import"./index-D9dZuxfj.js";import"./emotion-react.browser.esm-Bwsi5GpN.js";const k={title:"Data Display/Button",component:p},o={args:{children:"Button",onClick:()=>null,isLoading:!1,variant:"solid",color:"primary",size:"medium",isDisabled:!1},argTypes:{variant:{options:["solid","outlined","text"],control:{type:"select"}},color:{options:["primary","secondary","error","contrast","accent"],control:{type:"select"}},size:{options:["medium","large","small"],control:{type:"select"}},isDisabled:{control:"boolean"}}},n={...o,render:({...c})=>t.jsx("div",{style:{width:"200px",height:"100px",backgroundColor:"#000000",padding:"20px"},children:t.jsx(p,{...c})})};var r,e,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    onClick: () => null,
+    isLoading: false,
+    variant: 'solid',
+    color: 'primary',
+    size: 'medium',
+    isDisabled: false
+  },
+  argTypes: {
+    variant: {
+      options: ['solid', 'outlined', 'text'],
+      control: {
+        type: 'select'
+      }
+    },
+    color: {
+      options: ['primary', 'secondary', 'error', 'contrast', 'accent'],
+      control: {
+        type: 'select'
+      }
+    },
+    size: {
+      options: ['medium', 'large', 'small'],
+      control: {
+        type: 'select'
+      }
+    },
+    isDisabled: {
+      control: 'boolean'
+    }
+  }
+}`,...(s=(e=o.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};var a,i,l;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  ...Default,
+  render: ({
+    ...props
+  }) => <div style={{
+    width: '200px',
+    height: '100px',
+    backgroundColor: '#000000',
+    padding: '20px'
+  }}>
+      <Button {...props} />
+    </div>
+}`,...(l=(i=n.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const B=["Default","buttonWithDarkBackground"];export{o as Default,B as __namedExportsOrder,n as buttonWithDarkBackground,k as default};
