72b9961a42effead9e4b9f9e5a9c5078df9b06b5
dt-dds <dt-dds@daimlertruck.com>
2025-11-20 17:32:59 +0000

    chore: update and publish Storybook

---
+++ assets/IconButton.stories-BM1i-kPM.js
@@ -0,0 +1,8 @@
+var c=Object.defineProperty;var a=(e,i)=>c(e,"name",{value:i,configurable:!0});import{j as o}from"./jsx-runtime-Cgz2XvFB.js";import{I as d}from"./Icon-BrO18bYS.js";import{I as r}from"./IconButton-BsV1J9zH.js";import"./index-CwbBKW39.js";import"./emotion-styled.browser.esm-Cgg3GKEJ.js";const m=["contrast","default"],p=["extra-small","small","medium","large","extra-large"],v={component:r,title:"Data Display/IconButton",argTypes:{isDisabled:{control:"boolean"},variant:{options:m,control:{type:"select"}},size:{options:p,control:{type:"select"}}},render:a(e=>o.jsx("div",{style:{backgroundColor:e.variant==="default"?"white":"black"},children:o.jsx(r,{...e,children:o.jsx(d,{code:"edit"})})}),"render")},t={args:{onClick:a(()=>null,"onClick"),isDisabled:!1,variant:"default",size:"large"}};var s,n,l;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    onClick: () => null,
+    isDisabled: false,
+    variant: 'default',
+    size: 'large'
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const j=["Default"];export{t as Default,j as __namedExportsOrder,v as default};
