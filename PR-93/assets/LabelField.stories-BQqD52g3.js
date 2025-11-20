e44ce2ab6a5ed880a37e99ef796f20da187e8298
dt-dds <dt-dds@daimlertruck.com>
2025-11-20 15:37:31 +0000

    chore: update and publish Storybook

---
+++ PR-93/assets/LabelField.stories-BQqD52g3.js
@@ -0,0 +1,11 @@
+var c=Object.defineProperty;var r=(s,a)=>c(s,"name",{value:a,configurable:!0});import{j as n}from"./jsx-runtime-Cgz2XvFB.js";import{I as m}from"./Icon-BrO18bYS.js";import{L as i}from"./LabelField-C0PWGpoP.js";import"./index-CwbBKW39.js";import"./emotion-styled.browser.esm-Cgg3GKEJ.js";import"./Typography-CEwLj33J.js";import"./typography-B3nKKPHm.js";const L={title:"Data Display/LabelField",component:i,argTypes:{},render:r(({children:s,...a})=>n.jsx(i,{...a,children:s}),"render")},e={args:{children:"My Label",isActive:!1,isDisabled:!1,isRequired:!1,hasError:!1,isFloating:!0,icon:n.jsx(m,{code:"info",size:"small"})}};var o,t,l;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'My Label',
+    isActive: false,
+    isDisabled: false,
+    isRequired: false,
+    hasError: false,
+    isFloating: true,
+    icon: <Icon code='info' size='small' />
+  }
+}`,...(l=(t=e.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,L as default};
