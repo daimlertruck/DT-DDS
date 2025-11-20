7464a4bcfde3aee502fb734a943687c0a4077e40
dt-dds <dt-dds@daimlertruck.com>
2025-11-20 00:52:18 +0000

    chore: update and publish Storybook

---
+++ PR-90/assets/Form.stories-V04YRk2f.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-CB_V9I5Y.js";import{T as l}from"./TextField-Deh_pzTF.js";import"./TextField.styled-Dy6mViiV.js";import{F as t}from"./Form-DObVhwcQ.js";import"./index-CTjT7uj6.js";import"./Icon-w916x_k_.js";import"./emotion-styled.browser.esm-DBghOYGd.js";import"./IconButton-UZ_MQ6PE.js";import"./LabelField-mvyT1FXM.js";import"./Typography-Bb7lZ69m.js";import"./index-D9dZuxfj.js";import"./Tooltip-D4Uhzfmc.js";import"./index-B4tIMv5T.js";import"./emotion-react.browser.esm-Bwsi5GpN.js";import"./index-BAMY2Nnw.js";import"./iframe-Cyo0rn5N.js";import"./z-index-DWgeqfVW.js";import"./index-XjDa9cP3.js";const z={title:"Data Display/Form",component:t},u=()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),o=u.bind({});o.args={};const F=({isDisabled:d,title:c,tooltip:b})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:c,tooltip:b,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:d,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),e=F.bind({});e.args={title:"",tooltip:"",isDisabled:!1};var s,a,i;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(i=(a=o.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var m,p,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const A=["Default","FormGroup"];export{o as Default,e as FormGroup,A as __namedExportsOrder,z as default};
