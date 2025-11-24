d7018ef0d6ac613338a8e16f1c91a005fa33f4b0
dt-dds <dt-dds@daimlertruck.com>
2025-11-24 17:27:07 +0000

    chore: delete PR preview for PR-99

--- PR-99/assets/Spinner.stories-Bor1T9RZ.js
+++
@@ -1,7 +0,0 @@
-import{S as a}from"./Spinner-CH5NdkzX.js";import"./jsx-runtime-Cgz2XvFB.js";import"./index-CwbBKW39.js";import"./index-juXVSS06.js";import"./emotion-styled.browser.esm-Cgg3GKEJ.js";import"./Typography-CEwLj33J.js";import"./typography-B3nKKPHm.js";import"./emotion-react.browser.esm-DSneuyqi.js";const u={title:"Data Display/Spinner",component:a,parameters:{backgrounds:{values:[{name:"black",value:"#000"}]}},argTypes:{size:{options:["extra-small","small","medium","large","extra-large"],control:{type:"select"}},colorScheme:{options:["positive","negative"],control:{type:"radio"}}}},e={args:{size:"medium",colorScheme:"positive",loadingText:""}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 'medium',
-    colorScheme: 'positive',
-    loadingText: ''
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,u as default};
