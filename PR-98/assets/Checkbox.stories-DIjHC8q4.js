c43a7e22d0c03ffeaa6a78b2d56997fffdf5f42e
dt-dds <dt-dds@daimlertruck.com>
2025-11-24 16:44:47 +0000

    chore: delete PR preview for PR-98

--- PR-98/assets/Checkbox.stories-DIjHC8q4.js
+++
@@ -1,8 +0,0 @@
-var l=Object.defineProperty;var a=(n,t)=>l(n,"name",{value:t,configurable:!0});import{C as i}from"./Checkbox-siqGYWeX.js";import"./jsx-runtime-Cgz2XvFB.js";import"./index-CwbBKW39.js";import"./Icon-BrO18bYS.js";import"./emotion-styled.browser.esm-Cgg3GKEJ.js";const u={title:"Data Display/Checkbox",component:i,argTypes:{}},e={args:{isDisabled:!1,isChecked:!1,children:"Label",onChange:a(()=>null,"onChange")}};var s,r,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    isDisabled: false,
-    isChecked: false,
-    children: 'Label',
-    onChange: () => null
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,u as default};
