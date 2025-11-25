e6d5cd1e9e1c226ef29407b4db54dec1dfc2a4a7
dt-dds <dt-dds@daimlertruck.com>
2025-11-25 19:07:44 +0000

    chore: update and publish Storybook

---
+++ PR-104/assets/index-juXVSS06.js
@@ -0,0 +1,5 @@
+var r=Object.defineProperty;var e=(t,s)=>r(t,"name",{value:s,configurable:!0});import{j as a}from"./jsx-runtime-Cgz2XvFB.js";import{f as d}from"./emotion-styled.browser.esm-Cgg3GKEJ.js";const n=d.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,c=e(({dataTestId:t,children:s,element:o="div",style:i})=>a.jsx(n,{as:o,"data-testid":t,style:i,children:s}),"Box");export{c as B};
