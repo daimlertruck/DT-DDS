a3e0455c646ddc2f92a0cb91b34db4f4e7070420
dt-dds <dt-dds@daimlertruck.com>
2025-11-26 18:45:51 +0000

    chore: delete PR preview for PR-106

--- PR-106/assets/Card.stories-BzltG8lU.js
+++
@@ -1,6 +0,0 @@
-var c=Object.defineProperty;var o=(s,i)=>c(s,"name",{value:i,configurable:!0});import{j as r}from"./jsx-runtime-Cgz2XvFB.js";import{T as d}from"./Typography-CEwLj33J.js";import{C as t}from"./Card-D4j-VUbS.js";import"./index-CwbBKW39.js";import"./typography-B3nKKPHm.js";import"./emotion-styled.browser.esm-Cgg3GKEJ.js";const j={title:"Data Display/Card",component:t,render:o(({title:s,description:i})=>r.jsxs(t,{children:[r.jsx(t.Title,{children:s}),r.jsx(t.Detail,{children:r.jsx(d,{color:"grey_100",children:i})})]}),"render")},e={args:{title:"Card Title",description:"This is a short description"}};var a,n,p;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    title: 'Card Title',
-    description: 'This is a short description'
-  }
-}`,...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,j as default};
