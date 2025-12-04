8d991a38a810eb162905156893012975f3abd727
Diogo Costa <diogo.costa@daimlertruck.com>
2025-12-04 15:42:28 +0000

    fix: inject external css into datepicker bundle
    css imported by dependency from
    node_modules is not allowed in next applications
    
    https: //nextjs.org/docs/messages/css-npm
    Signed-off-by: Diogo Costa <diogo.costa@daimlertruck.com>

--- packages/react-packages/date-picker/tsup.config.ts
+++ packages/react-packages/date-picker/tsup.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   entry: ['index.ts'],
   format: ['esm', 'cjs'],
   external: [/@emotion.*/, 'react'],
+  noExternal: [/react-day-picker.*\.css$/],
   dts: true,
+  injectStyle: true,
 });
