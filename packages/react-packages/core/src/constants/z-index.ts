e4b6d5762b7a5fe04e75c8b0a535ec4599182e0c
Bernardo Pedrosa <bernardo.quirino@hotmail.com>
2025-12-09 15:29:42 +0000

    feat: increase dropdown z index
    Signed-off-by: Bernardo Pedrosa <bernardo.quirino@hotmail.com>

--- packages/react-packages/core/src/constants/z-index.ts
+++ packages/react-packages/core/src/constants/z-index.ts
@@ -1,8 +1,8 @@
 export const TABLE_STICKY_COLUMN_Z_INDEX = 1;
 export const TABLE_HEADER_Z_INDEX = 2;
-export const DROPDOWN_MENU_Z_INDEX = 3;
-export const TOOLTIP_Z_INDEX = 11;
 export const BACKDROP_Z_INDEX = 9;
-export const MODAL_Z_INDEX = 10;
 export const DRAWER_Z_INDEX = 9;
-export const TOAST_Z_INDEX = 10;
+export const MODAL_Z_INDEX = 10;
+export const DROPDOWN_MENU_Z_INDEX = 11;
+export const TOAST_Z_INDEX = 12;
+export const TOOLTIP_Z_INDEX = 13;
