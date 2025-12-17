d99adf4aea570bc75171c0e339dfbcefbb834ee2
dt-dds <dt-dds@daimlertruck.com>
2025-12-17 18:34:35 +0000

    chore: delete PR preview for PR-142

--- PR-142/assets/index--ly5R4Br.js
+++
@@ -1,5 +0,0 @@
-var a=Object.defineProperty;var e=(t,s)=>a(t,"name",{value:s,configurable:!0});import{j as d}from"./jsx-runtime-Cgz2XvFB.js";import{f as n}from"./emotion-styled.browser.esm-Cgg3GKEJ.js";const x=n.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`,f=e(({dataTestId:t,children:s,element:o="div",style:i,...r})=>d.jsx(x,{as:o,"data-testid":t,style:i,...r,children:s}),"Box");export{f as B};
