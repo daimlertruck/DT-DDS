32f7e205772ea2af686df58568e0c3bc5e165a60
dt-dds <dt-dds@daimlertruck.com>
2025-12-22 16:50:01 +0000

    chore: update and publish Storybook

---
+++ PR-146/assets/Card.stories-CgH5BdHW.js
@@ -0,0 +1,6 @@
+var c=Object.defineProperty;var o=(s,i)=>c(s,"name",{value:i,configurable:!0});import{j as r}from"./jsx-runtime-Cgz2XvFB.js";import{T as d}from"./Typography-Dk4smw7A.js";import{C as t}from"./Card-DR9chi3j.js";import"./index-CwbBKW39.js";import"./emotion-styled.browser.esm-CIRWz9XU.js";import"./typography-B3nKKPHm.js";const j={title:"Data Display/Card",component:t,render:o(({title:s,description:i})=>r.jsxs(t,{children:[r.jsx(t.Title,{children:s}),r.jsx(t.Detail,{children:r.jsx(d,{color:"grey_100",children:i})})]}),"render")},e={args:{title:"Card Title",description:"This is a short description"}};var a,n,p;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    title: 'Card Title',
+    description: 'This is a short description'
+  }
+}`,...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,j as default};
