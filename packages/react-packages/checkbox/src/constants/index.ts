01badd4a4d585432193b9a4151ad65088cdcf6d4
Masoud Soleymani <msoleym@tbdir.net>
2025-12-22 23:01:43 +0000

    fix: checkbox label alignment
    Signed-off-by: Masoud Soleymani <msoleym@tbdir.net>

--- packages/react-packages/checkbox/src/constants/index.ts
+++ packages/react-packages/checkbox/src/constants/index.ts
@@ -1,4 +1,13 @@
+import { CheckboxSize } from '../types';
+
 export const SIZES = {
   small: 20,
   large: 24,
 } as const;
+
+export const LABEL_HEIGHT_BUFFER = 1;
+
+export const LABEL_LINE_HEIGHT: Record<CheckboxSize, number> = {
+  small: 16,
+  large: 20,
+};
