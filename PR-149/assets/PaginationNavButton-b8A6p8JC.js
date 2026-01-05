31f481ee4affcceec5fc3f773e173d70797e962a
dt-dds <dt-dds@daimlertruck.com>
2026-01-05 12:19:44 +0000

    chore: update and publish Storybook

---
+++ PR-149/assets/PaginationNavButton-b8A6p8JC.js
@@ -0,0 +1,124 @@
+var x=Object.defineProperty;var a=(t,n)=>x(t,"name",{value:n,configurable:!0});import{j as i}from"./jsx-runtime-Cgz2XvFB.js";import{r as g}from"./index-CwbBKW39.js";import{I as $}from"./Icon-BWIy_dZb.js";import{T as d}from"./Tooltip-Dk46u24W.js";import{f as e,j as r}from"./emotion-styled.browser.esm-CIRWz9XU.js";const E=[10,20,50,100],k=1,o=24,y=141,m=200,_=e("div",{shouldForwardProp:a(t=>r(t)&&!t.startsWith("$"),"shouldForwardProp")})`
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: ${({$shouldCenterItems:t})=>t?"center":"flex-end"};
+  width: 100%;
+  gap: 12px;
+
+  @media screen and (min-width: ${({theme:t})=>t.breakpoints.mq3}px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`,j=e("div",{shouldForwardProp:a(t=>r(t)&&!t.startsWith("$"),"shouldForwardProp")})`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: ${({$shouldCenterItems:t})=>t?"center":"flex-start"};
+  gap: 12px;
+
+  @media screen and (min-width: ${({theme:t})=>t.breakpoints.mq3}px) {
+    width: auto;
+    justify-content: center;
+    flex-shrink: 0;
+  }
+`,N=e.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 10px 0;
+  flex-shrink: 0;
+  flex-wrap: nowrap;
+`,b=e.button`
+  ${({theme:t})=>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: ${o}px;
+    height: ${o}px;
+    padding: 0;
+    border: none;
+    position: relative;
+    background-color: transparent; 
+    color: ${t.palette.content.default};
+    cursor: pointer;
+
+    &:disabled {
+      i {
+        color: ${t.palette.content.light};
+      }
+      cursor: not-allowed;
+    }
+
+    &:hover:not(:disabled) {
+      background-color: ${t.palette.informative.light};
+    }
+
+    &:focus-visible {
+      outline: 2px solid ${t.palette.primary.default};
+      outline-offset: 1px;
+    }
+  `}
+`,W=e("button",{shouldForwardProp:a(t=>r(t)&&!t.startsWith("$"),"shouldForwardProp")})`
+  ${({theme:t,$isActive:n})=>`
+    ${t.fontStyles.bodyXsBold};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: ${o}px;
+    height: ${o}px;
+    border: none;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+    background-color: transparent; 
+    color: ${t.palette.content.default};
+    cursor: pointer;
+
+    &:focus-visible {
+      outline: 2px solid ${t.palette.primary.default};
+      outline-offset: 1px;
+      transition: none;
+    }
+
+    &:hover {
+      background-color: ${t.palette.informative.light};
+    }
+
+    ${n&&`
+        background-color: ${t.palette.primary.default};
+        color: ${t.palette.content.contrast};
+        cursor: default;
+
+        &:hover {
+          background-color: ${t.palette.primary.default};
+        }
+      `}
+  `}
+`,B=e.span`
+  ${({theme:t})=>`
+    ${t.fontStyles.bodyMdRegular};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: ${o}px;
+    height: ${o}px;
+    color: ${t.palette.content.default};
+    user-select: none;
+  `}
+`,R=e.div`
+  ${({theme:t})=>`
+    width: ${y}px;
+
+    label:empty {
+      display: none;
+    }
+
+    @media screen and (min-width: ${t.breakpoints.mq3}px) {
+      width: ${m}px;
+    }
+  `}
+`,A=e.span`
+  ${({theme:t})=>`
+    ${t.fontStyles.bodySmRegular};
+    white-space: nowrap;
+  `}
+`,h=g.memo(({ariaLabel:t,tooltipLabel:n,icon:c,targetPage:p,isDisabled:s,testId:u,onClick:f})=>{const l=i.jsx(b,{"aria-label":t,"data-testid":u,disabled:s,onClick:a(()=>f(p),"onClick"),children:i.jsx($,{code:c,size:"small"})});return s?l:i.jsxs(d,{children:[l,i.jsx(d.Content,{background:"full",direction:"top",children:n})]})});h.displayName="PaginationNavButton";export{k as F,j as I,N,_ as P,B as T,R as a,E as b,A as c,h as d,W as e};
