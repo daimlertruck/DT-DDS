3c394791f2f85f56af594e55ebadd8d7a9a4919d
dt-dds <dt-dds@daimlertruck.com>
2026-01-10 12:36:37 +0000

    chore: update and publish Storybook

---
+++ PR-157/assets/Form.stories-DW1XTEPq.js
@@ -0,0 +1,17 @@
+var F=Object.defineProperty;var i=(s,a)=>F(s,"name",{value:a,configurable:!0});import{j as r}from"./jsx-runtime-Cgz2XvFB.js";import{T as l}from"./TextField-BvxdrW2r.js";import{F as t}from"./Form-DNaTTVJG.js";import"./index-CwbBKW39.js";import"./Icon-BWIy_dZb.js";import"./emotion-styled.browser.esm-CIRWz9XU.js";import"./IconButton-DqfnmDe3.js";import"./LabelField-BrIM5Qam.js";import"./Typography-Dk4smw7A.js";import"./typography-B3nKKPHm.js";import"./index-Cf7F2iyz.js";import"./Tooltip-COrdlPvZ.js";import"./index-CjZasqJy.js";import"./emotion-react.browser.esm-BzOLYAw5.js";import"./index-6Tx4pmSd.js";import"./iframe-D-8qLqyo.js";import"./z-index-C44GOntP.js";import"./index-DV-VXEUn.js";const C={title:"Data Display/Form",component:t},x=i(()=>r.jsx(t,{children:r.jsx(l,{label:"label 1"})}),"Template"),o=x.bind({});o.args={};const j=i(({isDisabled:s,title:a,tooltip:u})=>r.jsxs(t,{children:[r.jsx(t.Group,{title:a,tooltip:u,children:r.jsx(l,{label:"label 1"})}),r.jsxs(t.Group,{isDisabled:s,children:[r.jsx(l,{label:"label 2"}),r.jsx(l,{label:"label 3"})]})]}),"TemplateFormGroup"),e=j.bind({});e.args={title:"",tooltip:"",isDisabled:!1};var m,p,n;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <Form>
+      <TextField label='label 1' />
+    </Form>;
+}`,...(n=(p=o.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};var d,c,b;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`({
+  isDisabled,
+  title,
+  tooltip
+}) => <Form>
+    <Form.Group title={title} tooltip={tooltip}>
+      <TextField label='label 1' />
+    </Form.Group>
+    <Form.Group isDisabled={isDisabled}>
+      <TextField label='label 2' />
+      <TextField label='label 3' />
+    </Form.Group>
+  </Form>`,...(b=(c=e.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};const H=["Default","FormGroup"];export{o as Default,e as FormGroup,H as __namedExportsOrder,C as default};
---
+++ PR-157/assets/Icon-BWIy_dZb.js
@@ -0,0 +1,27 @@
+var l=Object.defineProperty;var r=(t,o)=>l(t,"name",{value:o,configurable:!0});import{j as f}from"./jsx-runtime-Cgz2XvFB.js";import{f as p,d as m}from"./emotion-styled.browser.esm-CIRWz9XU.js";const c={"extra-small":"12px",small:"16px",medium:"20px",large:"24px","extra-large":"32px"},d=p.i`
+  ${({size:t,theme:o,variant:e,color:n})=>`
+    font-family: DTUI-icons-${o.icons};
+    font-weight: normal;
+    font-style: normal;
+    font-size: ${c[t]};
+    display: inline-block;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    font-variation-settings: 'FILL' ${e==="filled"?1:0};
+    color: ${n};
+
+    /* Support for all WebKit browsers. */
+    -webkit-font-smoothing: antialiased;
+    /* Support for Safari and Chrome. */
+    text-rendering: optimizeLegibility;
+  
+    /* Support for Firefox. */
+    -moz-osx-font-smoothing: grayscale;
+  
+    /* Support for IE. */
+    font-feature-settings: 'liga';
+  `}
+`,h=r(({code:t,color:o,dataTestId:e,size:n="large",style:i,variant:a="outlined"})=>{const s=m();return f.jsx(d,{color:o??s.palette.content.default,"data-testid":e??"icon",size:n,style:i,variant:a,children:t})},"Icon");export{h as I,c as f};
